cb27630e5d29674b6dd0bf19762ecf1de3944137
Rawan Eldamanhory <rawaneldamanhory@gmail.com>
2026-01-09 14:07:30 +0200

    Initial commit: Singleton design pattern implementation in C#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public sealed class Singleton
+    {
+        private static Singleton _instance;
+        private static readonly object _lock = new object();
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            Console.WriteLine(s1 == s2); // True
+            Console.ReadLine();
+        }
+    }
+}
